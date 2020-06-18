687db6fd0995934693cdde13aa83b7771da376eb
admin <d.art.shishkin@gmail.com>
2020-06-18 22:15:13 +0300

    Added SFG Brewery Application

--- src/test/java/com/artarkatesoft/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/com/artarkatesoft/brewery/TsbbSfgBreweryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.artarkatesoft.brewery.tsbbsfgbrewery;
+package com.artarkatesoft.brewery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
